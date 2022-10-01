7a6d8dc9072baa1eba96fa572d5673a0261e78f5
jhoisz <jvrda@ufpi.edu.br>
2022-10-01 15:36:17 -0300

    main

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import sys
-from escalonamento import *
+from fcfs import *
+from sstf import *
+from elevador import *
 
-print(fcfsFunction(posicaoInicial))
-print(sstfFunction(posicaoInicial))
-print(elevador(posicaoInicial, limite))
+# limite do braço do disco
+limite = None
+# posição inicial do disco
+posicaoInicial = None
+# lista das demais posições
+posicoes = []
+
+
+# tratamento de entrada
+for linha in sys.stdin:
+    linhaFormatada = int(linha.replace('\n', ''))
+    if (limite == None):
+        limite = linhaFormatada
+    elif (posicaoInicial == None):
+        posicaoInicial = linhaFormatada
+    else:
+        posicoes.append(linhaFormatada)
+
+
+print(fcfsFunction(posicaoInicial, posicoes))
+print(sstfFunction(posicaoInicial, posicoes))
+print(elevador(posicaoInicial, limite, posicoes))
